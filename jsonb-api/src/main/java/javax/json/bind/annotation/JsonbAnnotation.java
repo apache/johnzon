196769cba402d3db8537592b86c1678ac5d204bd
Mark Struberg <struberg@apache.org>
2017-03-31 13:09:56 +0200

    JsonbAnnotation is @Documented

--- jsonb-api/src/main/java/javax/json/bind/annotation/JsonbAnnotation.java
+++ jsonb-api/src/main/java/javax/json/bind/annotation/JsonbAnnotation.java
@@ -18,9 +18,11 @@
  */
 package javax.json.bind.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JsonbAnnotation {
 }
