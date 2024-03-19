baa4f3d47f79b048b5d473124e2e479fe8b8ec6c
Heewon Lee <94441510+pingpingy1@users.noreply.github.com>
2024-03-19 22:44:02 +0900

    Guard Mapper.write against null writer (#123)
    * Expose anonymous classes to DelegatingX classes
    
    Applies the suggestions by @jungm and @rmannibucau
    
    * Modify code style

--- johnzon-mapper/src/main/java/org/apache/johnzon/mapper/internal/Streams.java
+++ johnzon-mapper/src/main/java/org/apache/johnzon/mapper/internal/Streams.java
@@ -21,194 +21,43 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
-import java.nio.CharBuffer;
 
 public final class Streams {
     private Streams() {
         // no-op
     }
 
-    public static Writer noClose(final Writer from) {
-        return new Writer() {
-            @Override
-            public void write(final int c) throws IOException {
-                from.write(c);
-            }
-
-            @Override
-            public void write(final char[] cbuf) throws IOException {
-                from.write(cbuf);
-            }
-
-            @Override
-            public void write(final char[] cbuf, final int off, final int len) throws IOException {
-                from.write(cbuf, off, len);
-            }
-
-            @Override
-            public void write(final String str) throws IOException {
-                from.write(str);
-            }
-
-            @Override
-            public void write(final String str, final int off, final int len) throws IOException {
-                from.write(str, off, len);
-            }
-
-            @Override
-            public Writer append(final CharSequence csq) throws IOException {
-                return from.append(csq);
-            }
-
-            @Override
-            public Writer append(final CharSequence csq, final int start, final int end) throws IOException {
-                return from.append(csq, start, end);
-            }
-
-            @Override
-            public Writer append(final char c) throws IOException {
-                return from.append(c);
-            }
-
-            @Override
-            public void flush() throws IOException {
-                from.flush();
-            }
-
+    public static Writer noClose(final Writer writer) {
+        return new DelegatingWriter(writer) {
             @Override
             public void close() throws IOException {
-                from.flush();
+                flush();
             }
         };
     }
 
-    public static Reader noClose(final Reader from) {
-        return new Reader() {
+    public static Reader noClose(final Reader reader) {
+        return new DelegatingReader(reader) {
             @Override
             public void close() throws IOException {
                 // no-op
             }
-
-            @Override
-            public int read(final CharBuffer target) throws IOException {
-                return from.read(target);
-            }
-
-            @Override
-            public int read() throws IOException {
-                return from.read();
-            }
-
-            @Override
-            public int read(final char[] cbuf) throws IOException {
-                return from.read(cbuf);
-            }
-
-            @Override
-            public int read(final char[] cbuf, final int off, final int len) throws IOException {
-                return from.read(cbuf, off, len);
-            }
-
-            @Override
-            public long skip(final long n) throws IOException {
-                return from.skip(n);
-            }
-
-            @Override
-            public boolean ready() throws IOException {
-                return from.ready();
-            }
-
-            @Override
-            public boolean markSupported() {
-                return from.markSupported();
-            }
-
-            @Override
-            public void mark(final int readAheadLimit) throws IOException {
-                from.mark(readAheadLimit);
-            }
-
-            @Override
-            public void reset() throws IOException {
-                from.reset();
-            }
         };
     }
-    public static OutputStream noClose(final OutputStream from) {
-        return new OutputStream() {
+    public static OutputStream noClose(final OutputStream outputStream) {
+        return new DelegatingOutputStream(outputStream) {
             @Override
             public void close() throws IOException {
-                from.flush();
-            }
-
-            @Override
-            public void write(final int b) throws IOException {
-                from.write(b);
-            }
-
-            @Override
-            public void write(final byte[] b) throws IOException {
-                from.write(b);
-            }
-
-            @Override
-            public void write(final byte[] b, final int off, final int len) throws IOException {
-                from.write(b, off, len);
-            }
-
-            @Override
-            public void flush() throws IOException {
-                from.flush();
+                flush();
             }
         };
     }
 
-    public static InputStream noClose(final InputStream from) {
-        return new InputStream() {
+    public static InputStream noClose(final InputStream inputStream) {
+        return new DelegatingInputStream(inputStream) {
             @Override
             public void close() throws IOException {
                 // no-op
-            }
-
-            @Override
-            public int read(final byte[] b) throws IOException {
-                return from.read(b);
-            }
-
-            @Override
-            public int read(final byte[] b, final int off, final int len) throws IOException {
-                return from.read(b, off, len);
-            }
-
-            @Override
-            public long skip(final long n) throws IOException {
-                return from.skip(n);
-            }
-
-            @Override
-            public int available() throws IOException {
-                return from.available();
-            }
-
-            @Override
-            public void mark(final int readlimit) {
-                from.mark(readlimit);
-            }
-
-            @Override
-            public void reset() throws IOException {
-                from.reset();
-            }
-
-            @Override
-            public boolean markSupported() {
-                return from.markSupported();
-            }
-
-            @Override
-            public int read() throws IOException {
-                return from.read();
             }
         };
     }
